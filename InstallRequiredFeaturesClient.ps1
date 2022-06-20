b3ba3bb7dceeb9f24160af0b068d29bcd01bb828
discozebedee <36518260+discozebedee@users.noreply.github.com>
2022-06-20 21:13:35 +0100

    Create InstallRequiredFeaturesClient.ps1

--- InstallRequiredFeaturesClient.ps1
+++ InstallRequiredFeaturesClient.ps1
@@ -1,5 +1,6 @@
-﻿Install-WindowsFeature	NET-Framework-Features
-Install-WindowsFeature	NET-Framework-Core
-Install-WindowsFeature	NET-Framework-45-Features
-Install-WindowsFeature	NET-Framework-45-Core
-Install-WindowsFeature	NET-Framework-45-ASPNET
+# Install-WindowsFeature	NET-Framework-Features
+# Install-WindowsFeature	NET-Framework-Core
+# Install-WindowsFeature	NET-Framework-45-Features
+# Install-WindowsFeature	NET-Framework-45-Core
+# Install-WindowsFeature	NET-Framework-45-ASPNET
+Set-TimeZone -Id "GMT Standard Time"
