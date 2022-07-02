defdebe56255a33b23751aa8aa4262bf9672fcc6
discozebedee <36518260+discozebedee@users.noreply.github.com>
2022-07-02 18:47:01 +0100

    Add files via upload

---
+++ languagescript.ps1
@@ -0,0 +1,8 @@
+# Set Locale, language etc. 
+& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"UKRegion.xml`""
+ 
+# Set Timezone
+& tzutil /s "GMT Standard Time"
+ 
+# Set languages/culture
+Set-Culture en-GB
